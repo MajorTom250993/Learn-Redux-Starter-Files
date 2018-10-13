23b9b47c5aa2b5936a56d40b6d90acbbe5786fba
Yonatan Rojas S <yonatanrojas09@gmail.com>
2018-10-13 16:19:07 -0500

    layout and component setup

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl'
+
+// Components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'))
